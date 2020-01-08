5ab72422bf2f7ff927e0ba52702779e9257d1857
ga-ni <ggaeun3075@gmail.com>
2020-01-08 23:21:57 +0900

    first commit \n - sample files\n - Syntax test files

---
+++ Syntax/template.js
@@ -0,0 +1,16 @@
+var name="9999";
+var letter = 'Lorem '+name+'ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
+\
+Ut enim ad minim veniam, quis nostrud exercitation '+name+'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse '+name+'cillum dolore eu fugiat nulla pariatur. \
+\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui '+name+'officia deserunt mollit anim id est laborum.'
+
+console.log(letter);
+
+name = "2222";
+var letter2 = `
+
+Lorem ${name}ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+Ut enim ad minim veniam, quis nostrud exercitation ${name}ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ${name}cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui ${name}officia deserunt mollit anim id est laborum.`
+
+console.log(letter2);
