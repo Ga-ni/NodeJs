7855c7a400500d07fbc797bbae1406336f20d9fa
YGE <yge0723@mindslab.ai>
2020-01-14 09:33:40 +0900

    게시글 삭제까지 완료

--- main.js
+++ main.js
@@ -60,7 +60,13 @@ var app = http.createServer(function (request, response) {
 					var list = templateList(fileList);
 					var template = templateHTML(title, list, 
 						`<h2>${title}</h2><p>${description}</p>`,
-						`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method="post"> <!-- onsubmit="js이용"-->
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>
+                        `);
 					response.writeHead(200);
 					response.end(template);
 				});
@@ -128,7 +134,42 @@ var app = http.createServer(function (request, response) {
 				response.end(template);
 			});
 		});
-	}
+    }
+    else if(pathname ==='/update_process'){
+        var body = '';
+		request.on('data', function(data){
+			body = body + data;
+		});
+		request.on('end', function(){
+            var post = qs.parse(body);
+            var id=post.id;
+			var title = post.title;
+            var description = post.description;
+            
+            fs.rename(`data/${id}`, `data/${title}`,function(err){
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+				    response.writeHead(302, {Location: `/?id=${title}`});
+				    response.end();
+			    });
+            });
+			
+		});
+    }
+    else if(pathname ==='/delete_process'){
+        var body = '';
+		request.on('data', function(data){
+			body = body + data;
+		});
+		request.on('end', function(){
+            var post = qs.parse(body);
+            var id=post.id;
+            fs.unlink(`data/${id}`, function(err){
+                response.writeHead(302, {Location: `/`});
+				    response.end();
+            })
+			
+		});
+    }
 	else {
 		response.writeHead(404);
 		response.end('Not found');
